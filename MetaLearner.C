f4ffe0139183a90c1db5df758d7e3efc7f73c716
John Detloff <jmdetloff@gmail.com>
2026-07-22 13:46:57 -0500

    Trim edgePresenceProb

--- MetaLearner.C
+++ MetaLearner.C
@@ -488,7 +488,7 @@ MetaLearner::start(int currFold)
 	// populates edgePriorMap
 	initEdgePriorMeta_All();
 
-	// populates edgeMap, edgePresenceProb, varNeighborhoodPrior, potentials
+	// populates edgeMap, varNeighborhoodPrior, potentials
 	initEdgeSet();
 
 	bool checkpointLoaded = false;
@@ -810,7 +810,6 @@ MetaLearner::initEdgeSet()
 			{
 				initPrior = 1-1e-6;
 			}
-			edgePresenceProb[edgeKey] = initPrior;
 			if (varNeighborhoodPrior.find(vID) == varNeighborhoodPrior.end())
 			{
 				varNeighborhoodPrior[vID] = log(1 - initPrior);
